c68688226d54884efe7814f93c3e2c9443be05c8
JaidinHamilton <45185343+JaidinHamilton@users.noreply.github.com>
2023-05-15 11:19:08 -0400

    Add files via upload

--- VisionVoice.py
+++ VisionVoice.py
@@ -1,9 +1,7 @@
-
 import cv2
 from transformers import CLIPProcessor, CLIPModel
 from gtts import gTTS
 from flask import Flask, request, send_file
-import requests
 
 # Load and preprocess images
 def load_image(image_path):
@@ -17,9 +15,10 @@ def generate_description(image):
     processor = CLIPProcessor.from_pretrained(model_name)
     model = CLIPModel.from_pretrained(model_name)
 
-    inputs = processor(images=image, return_tensors="pt", padding=True)
-    logits = model(**inputs).logits_per_image
-    probs = logits.softmax(dim=-1)
+    # Adding an extra dimension to simulate a batch
+    inputs = processor(text=["a description for my image"], images=[image], return_tensors="pt", padding=True)
+    logits_per_image = model(**inputs).logits_per_image
+    probs = logits_per_image.softmax(dim=-1)
     description = processor.decode(probs.argmax(dim=-1)[0])
 
     return description
@@ -28,6 +27,11 @@ def generate_description(image):
 def text_to_speech(text, output_filename):
     tts = gTTS(text)
     tts.save(output_filename)
+
+# Write the description to a text file
+def write_to_text_file(text, filename):
+    with open(filename, 'w') as f:
+        f.write(text)
 
 # Set up a Flask server
 app = Flask(__name__)
@@ -39,9 +43,15 @@ def image_to_audio():
     
     image = load_image('input_image.jpg')
     description = generate_description(image)
-    text_to_speech(description, 'output_audio.mp3')
+    print(f"Generated description: {description}")  # Debug print
     
-    return send_file('output_audio.mp3', as_attachment=True)
+    if description:  # Check if description is not empty
+        # Write the description to a text file
+        write_to_text_file(description, 'output_text.txt')
+        text_to_speech(description, 'output_audio.mp3')
+        return send_file('output_audio.mp3', as_attachment=True)
+    else:
+        return "No description generated", 500
 
 if __name__ == '__main__':
     app.run()
